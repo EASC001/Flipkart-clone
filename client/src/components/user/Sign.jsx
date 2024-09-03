4b5a5cc509d6076231cfb4534c45bf37ca965a6d
Karthick1024 <karthickcs10124@gmail.com>
2024-09-03 14:30:28 +0530

    change

--- client/src/components/user/Sign.jsx
+++ client/src/components/user/Sign.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import "./sign.css"
 import { Link } from 'react-router-dom'
 
@@ -8,7 +8,7 @@ const Sign = () => {
             <div className='parent-contain'>
                 <div className='welcome'>
                     <div className='sign'>
-                        <h2>Looks like you're new here!</h2>
+                        <h2>Looks like you re new here!</h2>
                         <h5>Sign up with your mobile number to get started</h5>
                     </div>
                 </div>
@@ -16,7 +16,7 @@ const Sign = () => {
                     <input className='input-form' type="text" placeholder='Enter Mobile Number' />
                     <label className='form__label' htmlFor="">Enter Mobile Number</label>
                     <p className='' style={{ color: '#878787' }}>
-                        By continuing, you agree to Flipkart's <a className='link-offset-2 link-underline link-underline-opacity-0' href="#">Terms of Use</a> and <a className='link-offset-2 link-underline link-underline-opacity-0' href="#">Privacy Policy</a>
+                        By continuing, you agree to Flipkarts <a className='link-offset-2 link-underline link-underline-opacity-0' href="#">Terms of Use</a> and <a className='link-offset-2 link-underline link-underline-opacity-0' href="#">Privacy Policy</a>
                     </p>
                     <div className='d-grid gap-2'>
                         <button className='sign-button'><Link to="/">CONTINUE</Link></button>
