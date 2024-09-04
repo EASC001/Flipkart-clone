1c4e2a6c5dee7c9e73d692e0cad8ca17885d3da2
Karthick1024 <karthickcs10124@gmail.com>
2024-09-04 11:00:20 +0530

    profile

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -1,20 +1,29 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 =======
 import ''
 >>>>>>> db4f089 (change nav)
+=======
+import React from 'react'
+>>>>>>> c67cee8 (profile)
 
 const Profile = () => {
   return (
     <div>
+<<<<<<< HEAD
 <<<<<<< HEAD
       <h1>hi users</h1>
 =======
 
       
 >>>>>>> db4f089 (change nav)
+=======
+      
+>>>>>>> c67cee8 (profile)
     </div>
   )
 }
 
 export default Profile
+
