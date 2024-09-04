239f62207fd973553462538396c572f584aa4227
Karthick1024 <karthickcs10124@gmail.com>
2024-09-04 11:19:26 +0530

    css

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 =======
 import ''
@@ -7,15 +8,28 @@ import ''
 =======
 import React from 'react'
 >>>>>>> c67cee8 (profile)
+=======
+import './profile.css'
+>>>>>>> 81d23bc (css)
 
 const Profile = () => {
   return (
     <div>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       <h1>hi users</h1>
 =======
 
+=======
+      <div className="container">
+        <p>Personal Information</p>
+        <div className="input">
+          <input type="text" />
+          <input type="text" />
+        </div>
+      </div>
+>>>>>>> 81d23bc (css)
       
 >>>>>>> db4f089 (change nav)
 =======
